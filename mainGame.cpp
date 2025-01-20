4f3ccc941ded4b0db510eeb8bd04d17b4220e576
Raltour <15610159165lmz@gmail.com>
2025-01-20 14:06:41 +0800

    修改main函数主体逻辑

--- mainGame.cpp
+++ mainGame.cpp
@@ -10,28 +10,28 @@
 
 
 int main() {
-
+	//新建一条蛇
 	Snake* mySnake = new Snake();
-
-
+	//初始化界面设置
 	initgraph(X_LENGTH, Y_LENGTH + BLOCK_WIDTH);
-
 	setbkcolor(RGB(35, 206, 250));
 	cleardevice();//背景
-
 	setfillcolor(RGB(240, 255, 240));
 	setlinecolor(RGB(240, 255, 240));
 	setlinestyle(PS_DASH, 3);
 	fillrectangle(1, 1, X_SIZE * BLOCK_WIDTH, BLOCK_WIDTH);//计分区域
 
-	block(0, 0);
-	block(0, 1);//绘制一节蛇身的函数（x,y)  范围0-29
+	//进入游戏逻辑循环
+	while (true) {
+		block(0, 0);
+		block(0, 1);//绘制一节蛇身的函数（x,y)  范围0-29
 
-	score(5);//计分函数
+		score(5);//计分函数
 
-	food(10, 10);
+		food(10, 10);
+	}
 
-	char a = getchar();
+
 	closegraph();
 
 	delete mySnake;

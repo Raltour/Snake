ce5533e0ce09e3a4551e2d7e4223d72d4b4f5c2d
ssyz-ix <shenyouzi@sjtu.edu.cn>
2025-02-13 22:50:09 +0800

    Update Board.h

--- Board.h
+++ Board.h
@@ -3,11 +3,10 @@
 
 void block(int xcoord, int ycoord);
 void food(int xcoord, int ycoord);
-void score(int score);
 
 void initBoard();
 
-void refreshBoard(Snake* snake, Food* food, int currentscore);
+void refreshBoard(Snake* snake, Food* food);
 
 bool hasKeyInput();
 char getKey();//有时间研究下怎么搞这个

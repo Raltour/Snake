fbfba395b80bda51b55385269a6d86fc63750d35
Mingze Li <15610159165lmz@gmail.com>
2025-02-14 13:55:01 +0800

    去除Board.h中的drawFood函数，该函数仅供Board内部调用

--- Board.h
+++ Board.h
@@ -2,14 +2,9 @@
 #define BOARD_H
 
 void block(int xcoord, int ycoord);
-void drawFood(int xcoord, int ycoord);
 
 void initBoard();
 
 void refreshBoard(Snake* snake, Food* food);
-
-bool hasKeyInput();
-char getKey();//有时间研究下怎么搞这个
-
 
 #endif // BOARD_H
